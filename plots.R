98102c47a939dd5841d1f27c8f40cde9ac298cad
huberseb <erdkreisvermessungsamt@gmail.com>
2025-11-08 18:50:18 +0000

    Created first polots to get familiar with ggplot2
    Ploted mandible vs. body length for each species

--- plots.R
+++ plots.R
@@ -1,30 +1,52 @@
-# we will plot stuff here
-
-
 library(ggplot2)
 library(ggthemes)
+library(patchwork)
 
 #### Biometrics ####
 
 # read in file
 # dec = comma!
-raw_multi <- read.delim(
-  file   = "multiple_carriers.csv",
-  sep  = ";",
-  dec = "," # <-- very important for comma decimals
+all_df <- read.delim(
+  file   = "all_ants_all_prey.csv",
+  sep  = ",",
+  dec = "." # <-- check if eng with "point "." or german with "," 
 )
 
 
-# Plot ant mandible vs. body length 
-
-
-ggplot(df, aes(x = length, y = width, color = ant.species)) +
-  geom_point(alpha = 0.7, size = 2) +             # Punkte mit leichter Transparenz
-  geom_smooth(method = "lm", se = TRUE, linewidth = 1) +  # lineare Regressionslinie mit Konfidenzintervall
+# Plot ant mandible vs. body length for BOTH species
+ggplot(all_df, aes(x = ant_length, y = ant_width, color = ant_species)) +
+  geom_point(alpha = 0.5, size = 1) +             # slightly transparent points
+  geom_smooth(method = "lm", se = TRUE, linewidth = 1) +  # linear regression line 
   theme_clean(base_size = 14) +
   labs(
     x = "Ant body length (mm)",
     y = "Ant body width (mm)",
     color = "Species",
     title = "Relationship between body length and width in Dorylus ants"
+  )
+
+# Fuck around - Want to change coulours but dosnt work for now...
+ggplot(all_df, aes(x = ant_length, y = ant_width, color = ant_species)) +
+  geom_point(size = 2, alpha = 0.5) +
+  geom_smooth(method = "lm", se = TRUE, linewidth = 1.2) +
+  scale_color_manual(values = c("D. wilverthi" = "firebrick3",
+                                "D. sjostedti" = "steelblue3")) +
+  theme_clean(base_size = 14) +
+  labs(
+    x = "Ant body length (mm)",
+    y = "Ant body width (mm)",
+    color = "Species",
+    title = "Body length–width relationship in two Dorylus species"
+  )
+
+# Plot ant mandible vs. body length for EACH species
+ggplot(all_df, aes(x = ant_length, y = ant_width)) +
+  geom_point(color = "darkred", alpha = 0.7, size = 1) +
+  geom_smooth(method = "lm", se = TRUE, color = "black") +
+  facet_wrap(~ ant_species) +
+  theme_clean(base_size = 14) +
+  labs(
+    x = "Ant body length (mm)",
+    y = "Ant body width (mm)",
+    title = "Length–width relationship by Dorylus species"
   )
