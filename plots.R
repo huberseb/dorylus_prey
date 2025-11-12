e8f4d46df2e1e2a5c1ae1e03850d68e9bddeff1f
huberseb <erdkreisvermessungsamt@gmail.com>
2025-11-12 22:19:49 +0000

    Starte ploting the LRM´s  Created plot for Ant weight vs prey weight
    To Do:
    change units in df from g to mg. Legend is allreadr   in mg
    Check if it is the right model used

--- plots.R
+++ plots.R
@@ -13,7 +13,7 @@ all_df <- read.delim(
 )
 
 
-# Plot ant mandible vs. body length for BOTH species
+# Plot ant mandible vs. body length for BOTH species###########################
 ggplot(all_df, aes(x = ant_length, y = ant_width, color = ant_species)) +
   geom_point(alpha = 0.5, size = 1) +             # slightly transparent points
   geom_smooth(method = "lm", se = TRUE, linewidth = 1) +  # linear regression line 
@@ -39,7 +39,7 @@ ggplot(all_df, aes(x = ant_length, y = ant_width, color = ant_species)) +
     title = "Body length–width relationship in two Dorylus species"
   )
 
-# Plot ant mandible vs. body length for EACH species
+# Plot ant mandible vs. body length for EACH species###########################
 ggplot(all_df, aes(x = ant_length, y = ant_width)) +
   geom_point(color = "darkred", alpha = 0.7, size = 1) +
   geom_smooth(method = "lm", se = TRUE, color = "black") +
@@ -50,3 +50,43 @@ ggplot(all_df, aes(x = ant_length, y = ant_width)) +
     y = "Ant body width (mm)",
     title = "Length–width relationship by Dorylus species"
   )
+
+#=============================================================================#
+                      #### Linear Regression models #### 
+#=============================================================================#
+
+#Ant weight vs prey weight (single workers)###################################
+#This is the plot showing the lm1
+#Regression line shoulde be for bothe species sinces species has no sig. effect
+
+ggplot(df_single, aes(x = log_ant_weight, y = log_prey_weight,
+                      color = ant_species)) +
+#sets points  
+  geom_point(alpha = 0.7, size = 0.7) +  
+  
+#regression line, se sets confidence area 
+  geom_smooth(method = "lm", se = TRUE, color = "black", linewidth = 0.8) +
+
+#add horizontel reference line   
+  geom_hline(yintercept = 0, linetype = "dashed", linewidth = 0.5, color = "red") +
+  
+#set colour scheme 
+  scale_color_manual(values = c("#E69F00", "#56B4E9")) 
+
+#Add description and legend
+  labs(
+    x = "Ant weight (log10, mg)",
+    y = "Prey weight (log10, mg)",
+    color = "Dorylus"
+  ) +
+  theme_classic(base_size = 14) +
+  theme(
+    legend.position = "right",
+    legend.title = element_text(size = 14),
+    legend.text = element_text(size = 12),
+    axis.title = element_text(size = 14),
+    axis.text = element_text(size = 12)
+  )
+  #TO DO: CONVERT g TO mg FOR NICER SCALING. (TILL THEN LEGEND WRONG)
+  
+
