ca441be60bdaa1aecf87b1865a4cda2b9ce32d06
huberseb <erdkreisvermessungsamt@gmail.com>
2025-11-12 22:21:41 +0000

    Calculated Linear Regression Models
    Used different main effects and calculated which model is most fitting.
    
    Added some librarys

--- statistical_analysis.R
+++ statistical_analysis.R
@@ -2,6 +2,8 @@ library(ggplot2)
 library(effectsize)
 library(tidyvers)
 library(dplyr)
+library(lmtest)
+library(performance)
 
 #reading in data
 df <- read.delim(
@@ -46,7 +48,7 @@ write.csv(df_log, "all_ants_log.csv")
 
 
 #==============================================================================
-         #### Calculating important values and indexes ####
+            #### Calculating important values and indexes ####
 #==============================================================================
 #reading in data
 df <- read.delim(
@@ -81,21 +83,25 @@ df_multi <- df %>%
 ############## Relative loading single (loading / ant weight)
 # relative measure of how heavily loaded ants are relative to their weight.
 # single carriers only
-df_single["relativ_loading"] <- df_single$ant_loading / df_single$ant_weight
+df_single["relativ_loading"] <- df_single$ant_loading / df_single$log_ant_weight
 
 
 ############## Relative loading multi (loading / ant weight)
 # relative measure of how heavily loaded ants are relative to their weight.
 # multi carriers only
-df_multi["realative_loading"] <- df_multi$ant_loading / df_multi$ant_loading
+df_multi["realative_loading"] <- df_multi$ant_loading / df_multi$log_ant_weight
 
 
-#### Size differences between ant species ####
 
-# Welch t-test 
+#==============================================================================
+                #### Size differences between ant species #### 
+#==============================================================================
+
+
+#Welch t-test 
 t.test(ant_length ~ ant_species, data = df)
 
-# Effektgröße (Cohen’s d)
+#Effektgröße (Cohen’s d)
 
 cohens_d(ant_length ~ ant_species, data = df)
 
@@ -104,9 +110,54 @@ hist(df$ant_length, main = "Length", col = "grey")
 hist(df$ant_width,  main = "Width",  col = "grey")
 hist(df$ant_weight, main = "Weight", col = "grey")
 
-# Box plot by species
+#Box plot by species
 boxplot(ant_length ~ ant_species, data = df, main = "Ant length by species")
 boxplot(ant_width ~ ant_species, data = df, main = "Ant width by species")
 boxplot(ant_weight~ ant_species, data = df, main = "Ant weight by species")
+
+
+
+#==============================================================================
+                     #### Linear Regression models #### 
+#==============================================================================
+
+#setting species and forest as factor (aka categorical variable)#
+df_single$ant_species <- factor(df_single$ant_species)
+df_single$forest_type <- factor(df_single$forest_type)
+
+#In prey_shape and ant_size are INF (when not log transformed data == 1.00)
+#We assume the index is just 0 here to get rid of errors 
+df_single <- df_single %>%
+  mutate(across(c(ant_size, prey_shape),
+                ~ ifelse(is.infinite(.), 0, .))) #if Inf/-Inf write 0 
+
+
+#Ant weight vs prey weight (single workers)###################################
+#base model without explaining variables
+lm1 <- lm(log_prey_weight ~ log_ant_weight, data = df_single) 
+
+summary(lm1)
+confint(lm1) # 95%-Convidence intervall
+
+#model with only main effects
+lm2 <- lm(log_prey_weight ~ log_ant_weight + ant_species + forest_type + 
+          prey_shape, data = df_single)
+
+summary(lm2)
+confint(lm2)
+AIC(lm1, lm2)  #compares models - Akaike Information Criterion
+
+plot(lm2) #standard plots to check homoskedasticity, linearity, leverage
+bptest(lm2) #Breusch-Pagan-test on homoskedasticity (is Varianz in Residuals constant)
+
+#model with interacting effect specis an main effects 
+#test this to see if slope is different when species are considered
+lm3 <- lm(log_prey_weight ~ log_ant_weight * ant_species + forest_type + 
+           prey_shape, data = df_single)
+
+summary(lm3)
+confint(lm3)
+AIC(lm1, lm2, lm3)
+anova(lm2, lm3)
 
 
