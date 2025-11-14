848421f545efb2f255b7bec0f5d952e011c58bc7
huberseb <erdkreisvermessungsamt@gmail.com>
2025-11-14 01:12:52 +0000

    Added plot for:
    ant length vs width
    
    fixed some code in relaive load vs carrier size

--- plots.R
+++ plots.R
@@ -2,7 +2,9 @@ library(ggplot2)
 library(ggthemes)
 library(patchwork)
 
-#### Biometrics ####
+#=============================================================================#
+                            #### Biometrics #### 
+#=============================================================================#
 
 # read in file
 # dec = comma!
@@ -13,11 +15,20 @@ all_df <- read.delim(
 )
 
 
-# Plot ant mandible vs. body length for BOTH species###########################
-ggplot(all_df, aes(x = ant_length, y = ant_width, color = ant_species)) +
-  geom_point(alpha = 0.5, size = 1) +             # slightly transparent points
-  geom_smooth(method = "lm", se = TRUE, linewidth = 1) +  # linear regression line 
+# Ant length vs. ants width for BOTH species###########################
+ant_length_width <- ggplot(all_df, aes(x = ant_length, y = ant_width, color = ant_species)) +
+  geom_point(alpha = 0.3, size = 1.5) +             # slightly transparent points
+  geom_smooth(aes(color = NULL),    # remove species grouping for the line
+              method = "lm", 
+              se = TRUE, 
+              linewidth = 1,
+              color = "grey9") + 
+  scale_x_log10() +     
+  scale_y_log10() + 
+  scale_colour_manual(values = c("D. wilverthi" = "#56B4E9",
+                                 "D. sjostedti" = "#E69F00")) +
   theme_clean(base_size = 14) +
+  theme(legend.position = "bottom", plot.title = element_text(size = 14)) +
   labs(
     x = "Ant body length (mm)",
     y = "Ant body width (mm)",
@@ -25,21 +36,29 @@ ggplot(all_df, aes(x = ant_length, y = ant_width, color = ant_species)) +
     title = "Relationship between body length and width in Dorylus ants"
   )
 
-# Fuck around - Want to change coulours but dosnt work for now...
-ggplot(all_df, aes(x = ant_length, y = ant_width, color = ant_species)) +
-  geom_point(size = 2, alpha = 0.5) +
-  geom_smooth(method = "lm", se = TRUE, linewidth = 1.2) +
-  scale_color_manual(values = c("D. wilverthi" = "firebrick3",
-                                "D. sjostedti" = "steelblue3")) +
+jpeg(filename = "Relationships length width ants.jpg", width = 20, 
+     height = 18, units = "cm", res = 300)
+ant_length_width
+dev.off()
+
+#Ant length for both species##################################################
+ggplot(all_df, aes(x = ant_length, colour = ant_species, fill =  ant_species)) +
+  geom_histogram(position = "identity", alpha = 0.5, bins = 30) +
+  scale_fill_manual(values = c("D. wilverthi" = "#56B4E9",
+                                "D. sjostedti" = "#E69F00")) +
+  scale_colour_manual(values = c("D. wilverthi" = "#56B4E9",
+                               "D. sjostedti" = "#E69F00")) +
+  guides(colour = "none") +  
   theme_clean(base_size = 14) +
+  theme(plot.title = element_text(hjust = 1)) + 
   labs(
-    x = "Ant body length (mm)",
-    y = "Ant body width (mm)",
-    color = "Species",
-    title = "Body length–width relationship in two Dorylus species"
+    x = "Ant body length [mm]",
+    y = "Frequency",
+    fill = "Dorylus species",
+    title = "Body length in two Dorylus species"
   )
 
-# Plot ant mandible vs. body length for EACH species###########################
+#Ant widht vs. body length for EACH species###########################
 ggplot(all_df, aes(x = ant_length, y = ant_width)) +
   geom_point(color = "darkred", alpha = 0.7, size = 1) +
   geom_smooth(method = "lm", se = TRUE, color = "black") +
@@ -51,7 +70,7 @@ ggplot(all_df, aes(x = ant_length, y = ant_width)) +
     title = "Length–width relationship by Dorylus species"
   )
 
-
+hist(df$prey_weight)
 
 
 #=============================================================================#
@@ -98,8 +117,8 @@ ggplot(df_single, aes(x = log_ant_weight, y = log_prey_weight,
 
   
 #Relative load vs. Carrier size (single workers)###############################
-#NOTHING WORKS HERE !!!!!!!!!!!!!!!!!!!!!
-  ggplot(df_single, aes(x = ant_size, y = relativ_loading,
+
+  ggplot(df_single, aes(x = log10(ant_weight), y = log10(relativ_loading),
                         color = ant_species)) +
     #sets points  
     geom_point(alpha = 0.7, size = 0.7) +  
@@ -108,18 +127,18 @@ ggplot(df_single, aes(x = log_ant_weight, y = log_prey_weight,
     geom_smooth(method = "lm", se = TRUE, color = "black", linewidth = 0.8) +
     
     #add horizontel reference line   
-    geom_hline(yintercept = 0, linetype = "dashed", linewidth = 0.5, color = "red") +
+    #geom_hline(yintercept = 0, linetype = "dashed", linewidth = 0.5, color = "red") +
     
     #set colour scheme 
-    scale_color_manual(values = c("#E69F00", "#56B4E9")) 
+    scale_color_manual(values = c("#E69F00", "#56B4E9")) +
   
   #Add description and legend
   labs(
-    x = "Ant size",
+    x = "Ant weight",
     y = "Relative loading",
     color = "Dorylus"
   ) +
-    theme_classic(base_size = 14) +
+    theme_classic() +
     theme(
       legend.position = "right",
       legend.title = element_text(size = 14),
